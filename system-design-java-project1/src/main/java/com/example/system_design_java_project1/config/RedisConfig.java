5405a253bf791e7517fcef6409c3978899c28e4b
Sumit-ops357 <sumitrathod22724@gmail.com>
2026-06-20 09:54:39 +0530

    Fixed the issue occured at Redis Config file

--- system-design-java-project1/src/main/java/com/example/system_design_java_project1/config/RedisConfig.java
+++ system-design-java-project1/src/main/java/com/example/system_design_java_project1/config/RedisConfig.java
@@ -10,11 +10,11 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Bean
-   public RedisConnectionFactory redisConnectionFactory()
-   {
-       return new LettuceConnectionFactory();
-   }
+//    @Bean
+//   public RedisConnectionFactory redisConnectionFactory()
+//   {
+//       return new LettuceConnectionFactory();
+//   }
 
    @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory){
